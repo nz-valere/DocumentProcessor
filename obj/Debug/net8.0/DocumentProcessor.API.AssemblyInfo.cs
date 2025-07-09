3210961615f4a3d8cf730b90a192381701df82d0
Valere Nz. <nz-valere>
2025-07-09 14:18:02 +0100

    updated gitignore

--- obj/Debug/net8.0/DocumentProcessor.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/DocumentProcessor.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentProcessor.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21681ddbdaac3848f211aa53d8c81f183530cbec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254ac4a1c8e36e799ec256f01cc9b303b591973e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentProcessor.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocumentProcessor.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
